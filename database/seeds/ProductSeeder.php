bdd792cb7fa62f66c21ddbddd69ae1b33351d257
David Hermo Blanco <davidhermo.tei@gmail.com>
2023-04-23 16:58:40 +0200

    add products seeds

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $products = [
+            [
+                'name' => 'bicicleta',
+                'description' => 'producto description',
+                'price' => '10'
+            ]
+        ];
+
+        foreach ($products as $productData) {
+            Product::create([
+                'name' => $productData['name'],
+                'description' => $productData['description'],
+                'price' => $productData['price'],
+            ]);
+        }
+    }
+}
