785453323867059a63743f451544dda524af3266
David Hermo Blanco <davidhermo.tei@gmail.com>
2023-04-29 08:51:49 +0200

    added shopping cart route

--- app/Http/Controllers/Cart/GetCartController.php
+++ app/Http/Controllers/Cart/GetCartController.php
@@ -6,6 +6,9 @@ use App\Http\Resources\CartResource;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Response;
+use App\Product;
+use App\Cart;
+use App\CartItem;
 
 class GetCartController extends Controller
 {
@@ -32,6 +35,41 @@ class GetCartController extends Controller
         $cart = new cartResource($this->getCartController->__invoke($request));
 
         return response($cart, 200);
+    }
+    public function add(Request $request)
+    {
+        $productId = $request->input('product_id');
+        $quantity = $request->input('quantity', 1);
+
+        // Buscar el producto por su id
+        $product = Product::findOrFail($productId);
+
+        // Obtener el carrito de la compra actual del usuario
+        $cart = Cart::where('status_id', 1)->first();
+
+        // Si no hay un carrito de la compra activo, crear uno nuevo
+        if (!$cart) {
+            $cart = new Cart;
+            $cart->status_id = 1; // 1 es el id del estado "activo"
+            $cart->save();
+        }
+
+        // Buscar si ya existe el producto en el carrito de la compra
+        $cartItem = CartItem::where('cart_id', $cart->id)->where('product_id', $product->id)->first();
+
+        // Si el producto ya existe, incrementar la cantidad
+        if ($cartItem) {
+            $cartItem->quantity += $quantity;
+            $cartItem->save();
+        } else { // Si el producto no existe, crear un nuevo ítem en el carrito
+            $cartItem = new CartItem;
+            $cartItem->cart_id = $cart->id;
+            $cartItem->product_id = $product->id;
+            $cartItem->quantity = $quantity;
+            $cartItem->save();
+        }
+
+        return redirect()->route('show-products');
     }
 
 
